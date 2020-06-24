1f7fa8957cbc0339779a11bb50b5a45cb08ec902
Carlos <ckcarlosdesign@gmail.com>
2020-06-24 13:14:46 -0600

    handle teardown when RP is not connected

--- tests/test_flows.py
+++ tests/test_flows.py
@@ -8,3 +8,14 @@ def test_add_to_cart(py):
 
     py.get('[data-testid="add-to-bag"]', timeout=1).click()
     assert py.contains("$00.11")
+
+
+def test_add_to_cart_xpath(py):
+    py.visit('https://jane.com')
+    py.getx('//*[@data-testid="deal-image"]').click()
+
+    for dropdown in py.findx('//select'):
+        dropdown.select(1)
+
+    py.getx('//*[@data-testid="add-to-bag"]', timeout=1).click()
+    assert py.contains("$00.11")
