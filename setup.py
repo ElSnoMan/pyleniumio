53a1ffe750d31c8d7af8a642f938ab06dc811931
Daniel Floyd <gleek.dan@gmail.com>
2020-11-01 13:28:41 -0700

    Added toml dev dependency, modified setup.py install_packages function

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 import pylenium as app
-import subprocess
-import json
+import toml
 
 
-def get_list_of_packages() -> list:
-    """ Get a list of current dependencies from pip via the cmd line """
-    output = subprocess.getoutput("pip list --not-required --format=json")
-    packages = []
-    not_needed = ['setuptools', 'pip']
-    for item in json.loads(output.split('\n')[0]):
-        if item['name'] not in not_needed:
-            packages.append(item['name'])
-
-    return packages
+def get_install_requirements() -> list:
+    """ Parses a pipfile and returns a list of package names along with a version if applicable """
+    pipfile = toml.load('Pipfile')
+    packages = pipfile.get('packages').items()
+    return ["{0}{1}".format(pkg, ver) if ver != "*"
+            else pkg for pkg, ver in packages]
 
 
 setup(
@@ -28,7 +23,7 @@ setup(
     description='The best of Selenium and Cypress in a single Python Package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=get_list_of_packages(),
+    install_requires=get_install_requirements(),
     data_files=[('', [
         'pylenium/scripts/pylenium.json',
         'pylenium/scripts/pytest.ini',
