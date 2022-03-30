fa5fd39de3431d989d7873c0918cd1d806f8cce9
Carlos Kidman <carlos@qap.dev>
2022-03-30 01:04:36 -0600

    Fix test_config_defaults

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,50 +1,25 @@
 def test_py_config_defaults(py_config):
     # driver settings
-    assert py_config.driver.browser == 'chrome'
-    assert py_config.driver.remote_url == ''
+    assert py_config.driver.browser == "chrome"
+    assert py_config.driver.remote_url == ""
     assert py_config.driver.wait_time == 10
     assert py_config.driver.page_load_wait_time == 0
     assert py_config.driver.options == []
-    assert py_config.driver.version == 'latest'
+    assert py_config.driver.version == "latest"
     assert py_config.driver.capabilities == {}
     assert py_config.driver.experimental_options is None
     assert py_config.driver.webdriver_kwargs == {}
+    assert py_config.driver.seleniumwire_options == {}
 
     # logging settings
     assert py_config.logging.screenshots_on is True
-    assert py_config.logging.pylog_level == 'info'
+    assert py_config.logging.pylog_level == "debug"
 
     # viewport settings
     assert py_config.viewport.maximize is True
     assert py_config.viewport.width == 1440
     assert py_config.viewport.height == 900
-    assert py_config.viewport.orientation == 'portrait'
-
-    # custom settings
-    assert py_config.custom is not None
-
-
-def test_py_config(py_config):
-    # driver settings
-    assert py_config.driver.browser == 'chrome'
-    assert py_config.driver.remote_url == ''
-    assert py_config.driver.wait_time == 10
-    assert py_config.driver.page_load_wait_time == 0
-    assert py_config.driver.options == []
-    assert py_config.driver.version == 'latest'
-    assert py_config.driver.capabilities == {}
-    assert py_config.driver.experimental_options is None
-    assert py_config.driver.webdriver_kwargs == {}
-
-    # logging settings
-    assert py_config.logging.screenshots_on is True
-    assert py_config.logging.pylog_level == 'info'
-
-    # viewport settings
-    assert py_config.viewport.maximize is True
-    assert py_config.viewport.width == 1440
-    assert py_config.viewport.height == 900
-    assert py_config.viewport.orientation == 'portrait'
+    assert py_config.viewport.orientation == "portrait"
 
     # custom settings
     assert py_config.custom is not None
