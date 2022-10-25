853d25001b734dcdf920533bdd71c61ba512d25b
Carlos Kidman <carlos@qap.dev>
2022-10-25 12:21:56 -0600

    add pyc and pys fixtures

--- tests/test_flows.py
+++ tests/test_flows.py
@@ -2,27 +2,28 @@ import pytest
 from pylenium.driver import Pylenium
 
 
-@pytest.fixture
-def sauce(py: Pylenium) -> Pylenium:
+@pytest.fixture(scope="session")
+def sauce(pys: Pylenium) -> Pylenium:
     """Login to saucedemo.com as standard user."""
-    py.visit("https://www.saucedemo.com/")
-    py.get("#user-name").type("standard_user")
-    py.get("#password").type("secret_sauce")
-    py.get("#login-button").click()
-    yield py
-    py.get("#react-burger-menu-btn").click()
-    py.get("#logout_sidebar_link").should().be_visible().click()
+    pys.visit("https://www.saucedemo.com/")
+    pys.get("#user-name").type("standard_user")
+    pys.get("#password").type("secret_sauce")
+    pys.get("#login-button").click()
+    yield pys
+    pys.get("#react-burger-menu-btn").click()
+    pys.get("#logout_sidebar_link").should().be_visible().click()
 
 
-def test_add_to_cart_css(sauce: Pylenium):
-    """Add an item to the cart. The number badge on the cart icon should increment as expected."""
-    sauce.get("[id*='add-to-cart']").click()
-    assert sauce.get("a.shopping_cart_link").should().have_text("1")
+class TestSauceDemo:
+    def test_add_to_cart_css(self, sauce: Pylenium):
+        """Add an item to the cart. The number badge on the cart icon should increment as expected."""
+        sauce.get("[id*='add-to-cart']").click()
+        assert sauce.get("a.shopping_cart_link").should().have_text("1")
 
 
-def test_add_to_cart_xpath(sauce: Pylenium):
-    """Add 6 different items to the cart. There should be 6 items in the cart."""
-    for button in sauce.findx("//*[contains(@id, 'add-to-cart')]"):
-        button.click()
-    sauce.getx("//a[@class='shopping_cart_link']").click()
-    assert sauce.findx("//*[@class='cart_item']").should().have_length(6)
+    def test_add_to_cart_xpath(self, sauce: Pylenium):
+        """Add 6 different items to the cart. There should be 6 items in the cart."""
+        for button in sauce.findx("//*[contains(@id, 'add-to-cart')]"):
+            button.click()
+        sauce.getx("//a[@class='shopping_cart_link']").click()
+        assert sauce.findx("//*[@class='cart_item']").should().have_length(6)
